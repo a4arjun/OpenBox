eb941f1b3e1cc73a933b57a88146a89bb7b00fc5
a4arjun <arjun.venugopal@hotmail.com>
2018-02-06 18:23:37 +0530

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'toyota');
+define('DB_PASSWORD', '');
 define('DB_NAME', 'openbox');
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($link === false){
